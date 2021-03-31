b44a8e8d8298fa29d28efde29ca2792889d8d61b
Juri <Chau-ying-kot@hotmail.com>
2021-03-31 17:57:34 +0200

    refactor

--- Labo_01.py
+++ Labo_01.py
@@ -1,30 +1,30 @@
+#  Chau Ying Kot, Adrian Mayo Cartes
 import time
-
 import vtk
 
 debuging = False
 
 headRadius = 5.
+sleepTime = 0.01
 
 # Nose
-cone = vtk.vtkConeSource()
-cone.SetHeight( 3.0 )
-cone.SetRadius( 1.0 )
-cone.SetResolution(10)
+nose = vtk.vtkConeSource()
+nose.SetHeight( 3.0 )
+nose.SetRadius( 1.0 )
+nose.SetResolution(100)
 
-coneMapper = vtk.vtkPolyDataMapper()
-coneMapper.SetInputConnection(cone.GetOutputPort())
+noseMapper = vtk.vtkPolyDataMapper()
+noseMapper.SetInputConnection(nose.GetOutputPort())
 
-coneActor = vtk.vtkActor()
-coneActor.SetMapper(coneMapper)
-coneActor.GetProperty().SetColor(255 / 255, 153 / 255, 51 / 255)
+noseActor = vtk.vtkActor()
+noseActor.SetMapper(noseMapper)
+noseActor.GetProperty().SetColor(255 / 255, 153 / 255, 51 / 255)
 
 # Head
-
 head = vtk.vtkSphereSource()
 head.SetRadius(headRadius)
-head.SetThetaResolution(10)
-head.SetPhiResolution(10)
+head.SetThetaResolution(100)
+head.SetPhiResolution(100)
 
 headMapper = vtk.vtkPolyDataMapper()
 headMapper.SetInputConnection(head.GetOutputPort())
@@ -34,11 +34,10 @@ headActor.SetMapper(headMapper)
 headActor.GetProperty().SetColor(1, 1, 1)
 
 # Body
-
 body = vtk.vtkSphereSource()
 body.SetRadius(7.)
-body.SetThetaResolution(10)
-body.SetPhiResolution(10)
+body.SetThetaResolution(100)
+body.SetPhiResolution(100)
 
 bodyMapper = vtk.vtkPolyDataMapper()
 bodyMapper.SetInputConnection(body.GetOutputPort())
@@ -48,11 +47,10 @@ bodyActor.SetMapper(bodyMapper)
 bodyActor.GetProperty().SetColor(1, 1, 1)
 
 # Eyes
-
 eyeLeft = vtk.vtkSphereSource()
 eyeLeft.SetRadius(1.)
-eyeLeft.SetThetaResolution(10)
-eyeLeft.SetPhiResolution(10)
+eyeLeft.SetThetaResolution(100)
+eyeLeft.SetPhiResolution(100)
 
 eyeLeftMapper = vtk.vtkPolyDataMapper()
 eyeLeftMapper.SetInputConnection(eyeLeft.GetOutputPort())
@@ -64,8 +62,8 @@ eyeLeftActor.GetProperty().SetColor(0, 0, 0)
 
 eyeRight = vtk.vtkSphereSource()
 eyeRight.SetRadius(1.)
-eyeRight.SetThetaResolution(10)
-eyeRight.SetPhiResolution(10)
+eyeRight.SetThetaResolution(100)
+eyeRight.SetPhiResolution(100)
 
 eyeRightMapper = vtk.vtkPolyDataMapper()
 eyeRightMapper.SetInputConnection(eyeRight.GetOutputPort())
@@ -78,12 +76,10 @@ eyeRightActor.GetProperty().SetColor(0, 0, 0)
 camera = vtk.vtkCamera()
 camera.SetFocalPoint(0, 0, 0)
 camera.SetPosition(0, 0, 120)
-camera.SetViewUp(0, 1, 0) # maybe not useful
-
 
 # Renderer
 renderer= vtk.vtkRenderer()
-renderer.AddActor(coneActor)
+renderer.AddActor(noseActor)
 renderer.AddActor(headActor)
 renderer.AddActor(bodyActor)
 renderer.AddActor(eyeLeftActor)
@@ -107,61 +103,60 @@ iren.SetRenderWindow(renWin)
 style = vtk.vtkInteractorStyleTrackballCamera()
 iren.SetInteractorStyle(style)
 
-
 iren.Initialize()
  
-# Init 
-coneActor.SetPosition(10, 0, 0)
-coneActor.RotateZ(-90)
+# Init position
+noseActor.SetPosition(10, 0, 0)
+noseActor.RotateZ(-90)
 
 headActor.SetPosition(-15,0,0)
 
 bodyActor.SetPosition(0,0,0)
 
-eyeLeftActor.SetPosition(3,10,4)
+eyeLeftActor.SetPosition(3,11,4)
 eyeLeftActor.SetVisibility(False)
 
-eyeRightActor.SetPosition(-3,10,4)
+eyeRightActor.SetPosition(-3,11,4)
 eyeRightActor.SetVisibility(False)
 
 
-# animations
+# Animations
 def noseMove():
     noseTransform = vtk.vtkTransform()
-    coneActor.SetUserTransform(noseTransform)
+    noseActor.SetUserTransform(noseTransform)
     
     for i in range(0, 90):
-        time.sleep(0.03)
+        time.sleep(sleepTime)
         
         noseTransform.RotateWXYZ(-1, 0, 1, 0)  # -1 deg on the world axis Y
         renWin.Render()
     
     for i in range(0, 90):
-        time.sleep(0.03)
+        time.sleep(sleepTime)
         
         noseTransform.RotateWXYZ(1, 0, 0, 1)  # 1 deg on the world axis Z. The coordinates change with the transformation
         renWin.Render()
 
     factor = 20 # to smooth the animation
-    for i in range(0, int(headRadius * factor)):
-        time.sleep(0.03)
+    for i in range(0, int(headRadius * factor)+ 10):
+        time.sleep(sleepTime)
 
         noseTransform.Translate(0, -1 / factor, 0)
         renWin.Render()
 
-def moveHeadAlone():
+def moveHead():
     headTransform = vtk.vtkTransform()
     headActor.SetUserTransform(headTransform)
 
     for i in range(0, 90):
-        time.sleep(0.03)
+        time.sleep(sleepTime)
 
         headTransform.RotateWXYZ(-1, 0, 0, 1)
         renWin.Render()
 
     factor = 20 # to smooth the animation
     for i in range(0, int(headRadius * factor)):
-        time.sleep(0.03)
+        time.sleep(sleepTime)
 
         headTransform.Translate(1 / factor, 0, 0)
         renWin.Render()
@@ -173,7 +168,7 @@ def setEyesVisibility() :
 def rollCamera(angle, sign = 1) :
     move = 1 * sign
     for i in range(0, angle):
-        time.sleep(0.03)
+        time.sleep(sleepTime)
 
         camera.Roll(move)
         renWin.Render()
@@ -182,7 +177,7 @@ def azimuthCamera(angle, sign = 1) :
     
     move = 1 * sign
     for i in range(0, angle):
-        time.sleep(0.03)
+        time.sleep(sleepTime)
 
         camera.Azimuth(move)
         renWin.Render() 
@@ -190,14 +185,14 @@ def azimuthCamera(angle, sign = 1) :
 def elevationCamera(angle, sign= 1) :
     move = 1 * sign
     for i in range(0, angle):
-        time.sleep(0.03)
+        time.sleep(sleepTime)
 
         camera.Elevation(move)
         renWin.Render() 
 
 def updateAll():
 
-    moveHeadAlone()
+    moveHead()
     noseMove()
     setEyesVisibility()
     rollCamera(360, -1)
